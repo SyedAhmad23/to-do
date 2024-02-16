c3c5ca275f38459bce335bb9cc5d5231bf9c8666
unknown <ahmadshah3236@gmail.com>
2024-02-16 13:05:55 +0500

    chore: Implemented user authentication functionalities

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
+import { store } from "@/app/store";
+import { ROUTES } from "@/constants/Routes";
 import "@/styles/globals.css";
+import { ThemeProvider, unstable_createMuiStrictModeTheme } from "@mui/material";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const theme = unstable_createMuiStrictModeTheme();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    if (!isLoggedIn && router.pathname !== ROUTES.login) {
+      router.push(ROUTES.login);
+    }
+  }, [router]);
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar/Navbar";
+import React from "react";
+
+const dashboard = () => {
+    return <div>
+        <Navbar />
+    </div>;
+};
+
+export default dashboard;
